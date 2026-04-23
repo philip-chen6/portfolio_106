92f32ef3e2bfc156b149cbabbf3b9e9a7cef93ac
Philip Chen <philipchen247@gmail.com>
2026-04-23 00:22:00 -0700

    finish lab 4

--- global.js
+++ global.js
@@ -57,7 +57,10 @@ document.body.insertAdjacentHTML(
 let select = document.querySelector(".color-scheme select");
 
 select.addEventListener("input", function (event) {
-  document.documentElement.style.setProperty("color-scheme", event.target.value);
+  document.documentElement.style.setProperty(
+    "color-scheme",
+    event.target.value,
+  );
   localStorage.colorScheme = event.target.value;
 });
 
@@ -67,4 +70,36 @@ if ("colorScheme" in localStorage) {
     localStorage.colorScheme,
   );
   select.value = localStorage.colorScheme;
+}
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON data:", error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = "h2") {
+  containerElement.innerHTML = "";
+  project.forEach((p) => {
+    const article = document.createElement("article");
+    article.innerHTML = `
+    <${headingLevel}>${p.title}</${headingLevel}>
+    <img src="${p.image}" alt="${p.title}">
+    <p>${p.description}</p>
+    <p><em>c. ${p.year}</em></p>
+    `;
+    containerElement.appendChild(article);
+  });
+}
+
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
